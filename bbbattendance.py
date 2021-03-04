eea31b698d366fd63819e1188da40b063dd6bfaa
Gerlando Lo Savio <gerlosgm@gmail.com>
2021-03-04 01:31:31 +0100

    Add command line interface

--- bbbattendance.py
+++ bbbattendance.py
@@ -29,11 +29,63 @@ def_user = ""
 ###################################################################
 #FUNCTIONS
 ###################################################################
+def gen_outfile_name(req_date, req_room, req_user):
+    """gen_outfile_name: Generate a string to use as a file name for the CSV
+    report file, based on criteria specified by the user, if any.
+    """
+    basename = def_output_basename
+    ext = ".csv"
+    # append user specified criteria to file name
+    for item in req_date, req_room, req_user:
+        if item != "":
+            basename = basename + "-" + item
+    filename = basename + ext
+    return filename
+
+def get_user_input(date, room, user, logfile, outfile):
+    """get_user_input: Read arguments from command line, and set defaults if
+    any parameter is missing.
+    Returns a list of parameters for subsequent processing.
+    """
+    desc = """Extract logs start and stop events for rooms and join and left
+    events for users from BigBlueButton log. Can filter events based on date,
+    room name, and user name.
+    """
+    epilog="""Without any option outputs all events occurred in the current day
+    from default log file. Since log files are often rotated, you may need to
+    specify which one to use.
+
+    Results are put in a CSV file, by default beginning with "bbb-report".
+    Columns output: Date,Time,Room,User,Event
+    """
+    parser = argparse.ArgumentParser(
+                description=desc, epilog=epilog)
+    parser.add_argument("date", type=str, nargs='?', default=date,
+                help="date of the events to extract, written like {} (default is today)".format(date))
+    parser.add_argument("-r", "--room", type=str, default=room,
+                help="room to search for")
+    parser.add_argument("-u", "--user", type=str, default=user,
+                help="user to search for")
+    parser.add_argument("-l", "--logfile", type=str, default=logfile,
+                help="log file to parse, default is {}".format(logfile))
+    parser.add_argument("-o", "--outfile", type=str, default=outfile,
+                help="output file to save parsed data, default is '{}-...'".format(outfile))
+    args = parser.parse_args()
+
+    if args.outfile == outfile:
+        req_outfile = gen_outfile_name(args.date, args.room, args.user)
+        print("User didn't provided any output file name, data will be saved to {}".format(req_outfile))
+    else:
+        req_outfile = args.outfile
+
+    return args.date, args.room, args.user, args.logfile, req_outfile
 
 
 ###################################################################
 # MAIN
 ###################################################################
 if __name__ == '__main__':
-    pass
-    
+    # Get user input, or use defaults if missing
+    req_date, req_room, req_user, logfile, outfile = get_user_input(
+                                    def_date, def_room, def_user,
+                                    def_logfile, def_output_basename)

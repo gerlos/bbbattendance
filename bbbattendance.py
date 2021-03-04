d07ba8f781c7dd16b8492414208c150f811c1c6a
Gerlando Lo Savio <gerlosgm@gmail.com>
2021-03-04 01:46:28 +0100

    Added License

--- bbbattendance.py
+++ bbbattendance.py
@@ -30,6 +30,20 @@ the `iso8601` module. To install it, use `pip3 install iso8601` or
 `apt install python3-iso8601` (on Debian and Ubuntu).
 """
 
+# Copyright 2021 Gerlando Lo Savio
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import argparse
 import csv
 import json
